8047f99a82bf85a3557094042bc182ef92869675
Anatoly Finchenko <finchenko777@gmail.com>
2023-07-15 07:44:22 +0300

    *HomeWork_1*

---
+++ script.js
@@ -0,0 +1,10 @@
+// Задание 1
+const cel = prompt("Введите градусы цельсия");
+const far = (9 / 5) * cel + 32;
+console.log(far);
+alert(far);
+
+// задание 2
+const name = "Anatoly";
+const admin = name;
+console.log(`значение переменной admin ${admin}`);
